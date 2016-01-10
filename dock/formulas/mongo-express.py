e2e5d87b76d33feee9003ba11304bdb00213eec4
Ben Ripkens <bripkens.dev@gmail.com>
2016-01-10 19:21:49 +0100

    Reuse container name in mongo-express

--- dock/formulas/mongo-express.py
+++ dock/formulas/mongo-express.py
@@ -3,9 +3,9 @@ import docker
 container_name = 'mongo-express'
 
 def run(args):
-  docker.force_stop('mongo-express')
-  docker.requires(dependent='mongo-express', dependency='mongodb')
+  docker.force_stop(container_name)
+  docker.requires(dependent=container_name, dependency='mongodb')
   docker.run(image='knickers/mongo-express:latest',
-             name='mongo-express',
+             name=container_name,
              link=[['mongodb', 'mongo']],
              publish=[8081])
